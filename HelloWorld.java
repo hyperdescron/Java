62ae6106b6477ae832d8cf5d02304e3824f009e3
devsynchdescron <devsynchdescron@gmail.com>
2023-09-09 21:45:40 +0300

    Create HelloWorld.java
    Fundamentals of Java

---
+++ HelloWorld.java
@@ -0,0 +1,46 @@
+
+
+    // This is single line comment 
+    // Comments are ignored by java and used to leave small notes for other developers
+
+    /*Multi
+     * line
+     * comment
+     */
+
+    // Public is an access modifier there are 4 access modifiers
+    // Which are public,protected,private and default
+
+    // Class is a blueprint for creating objects
+    // If you consider Humans as a class an object would be Jason or Alice
+    // An object has state and behaviour
+    // State of an object is its attributes and called instance variables
+    // An example for Human class could be height attribute
+    // Behaviour of an object is called methods
+    // An example for Human class could be sleep behaviour
+
+    // HelloWorld is the name of our class it has to match the file name
+    // Here we have the reserved keyword static
+    // If a variable or methods declared as static it means it belongs to the class rather than object
+    // All objects of a class shares that classes static methods and variables
+    // Reserved keywords cannot be used as identifier(variable) name
+    // Here return type is declared as void
+    // Void means the method doesn't return anything
+
+    // And we have method named main
+    // This is the entry point to every Java program
+    // The program begins initialization from this main method
+    // It accepts an array of String
+
+    // System is a built in class you can observe this classes source code using ctrl+F12 shortcut
+    // And finally we have println() method which prints out the value inside the parenthesis
+   
+
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    
+       
+    }
+}
