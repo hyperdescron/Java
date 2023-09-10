c45944e2992680ec1e11d174258502083ff07f4c
devsynchdescron <devsynchdescron@gmail.com>
2023-09-11 01:13:59 +0300

    Update Exceptions.java

--- Exceptions.java
+++ Exceptions.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Exceptions {
+
+    
     public static void main(String[] args) {
+        
 
         // Exceptions are used to handle unwanted or unexpected event
         // Division by 0 is a perfect example to demonstrate
@@ -21,5 +28,45 @@ public class Exceptions {
         } catch (ArithmeticException e) {
             System.out.println("Cannot divide by zero " + e);
         }
+
+        try{
+            int[] arr = new int[5];
+            arr[5] = 2;
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Index out of bounds");
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            int num = Integer.parseInt("null");
+        }catch(NumberFormatException e){
+            System.out.println("Number format exception");
+            System.out.println(e.getMessage());
+        } 
+
+        try {
+            file();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            System.out.println(e.getMessage());
+        }finally{
+            System.out.println("Try-catch finished");
+        }
+
+        System.out.println("Program continues...");
+        
+        
+
+        
     }
+
+    public static void file() throws FileNotFoundException{
+        FileReader file = new FileReader("/abc.txt");
+        BufferedReader br = new BufferedReader(file);
+        throw new FileNotFoundException();
+    }
+
+  
+    
+    
 }
